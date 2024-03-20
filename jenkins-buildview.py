8901c01bd98712ee21977ac0ee83a0604ee74b4c
James Lloyd <james.lloyd@tennet.eu>
2024-03-20 17:00:52 +0100

    Add beginnings of console

--- jenkins-buildview.py
+++ jenkins-buildview.py
@@ -8,7 +8,7 @@ import configparser
 from os import path
 from textual import work
 from textual.app import App, ComposeResult
-from textual.widgets import Header, Footer, Static, Tree
+from textual.widgets import Header, Footer, Static, Tree, Log
 from textual.widget import Widget
 from textual.reactive import reactive
 
@@ -49,11 +49,14 @@ class BuildDisplay(Static):
 
     def compose(self) -> ComposeResult:
         yield Tree(id="build_tree", label="No build")
+        log = Log(id="step_log")
+        log.styles.display = "none"
+        yield log
 
     def watch_build(self, old_build, new_build) -> None:
         tree = self.query_one("#build_tree", Tree)
-        tree.root.label = new_build["name"] if new_build is not None else "No build"
         if new_build:
+            tree.root.label = f"Build {new_build['name']}: {new_build['status']}"
             tree.root.remove_children()
             tree.root.expand()
             for stage in new_build["stages"]:
@@ -61,13 +64,19 @@ class BuildDisplay(Static):
                 match stage['status']:
                     case 'SUCCESS': label = "[green]" + label
                     case 'FAILED': label = "[red]" + label
+                    case 'IN_PROGRESS': label = "[white bold]" + label
 
                 leaf = tree.root.add_leaf(label)
-            # for stage in new_build["stages"]:
-            #    if 'error' in stage:
-            #        print(stage['name'], stage['status'], stage['error']['message'])
-            #    else:
-            #        print(stage['name'], stage['status'])
+        else:
+            tree.root.label = "No build"
+            
+
+
+    def on_tree_node_selected(self, message):
+        log = self.query_one("#step_log")
+        log.styles.display = "block"
+        log.clear()
+        log.write_line(str(message.node.label))
 
 
 
@@ -76,18 +85,23 @@ class JenkinsBuildViewApp(App):
 
     BINDINGS = [("d", "toggle_dark", "Toggle dark mode"),
                 ("q", "quit", "Quit"),
-                ("r", "refresh", "Refresh")]
+                ("b", "build", "Build")]
 
     url = reactive("")
     latest_build_url = reactive("")
+
+    def __init__(self, initial_url=None):
+        super().__init__()
+        auth = (os.environ["USERNAME"], os.environ["TOKEN"])
+        self.client =  httpx.Client(verify="/etc/ssl/certs/ca-bundle.crt", auth=auth)
 
     def compose(self) -> ComposeResult:
         """Create child widgets for the app."""
         yield Footer()
         yield BuildDisplay(id="build_display")
 
-    def on_mount(self) -> None:
-        self.set_interval(5, self.update_latest_build)
+    async def on_mount(self) -> None:
+        self.update_latest_build() 
 
     def action_toggle_dark(self) -> None:
         """An action to toggle dark mode."""
@@ -96,37 +110,30 @@ class JenkinsBuildViewApp(App):
     @work(exclusive=True)
     async def update_latest_build(self) -> None:
         auth = (os.environ["USERNAME"], os.environ["TOKEN"])
-        with httpx.Client(verify="/etc/ssl/certs/ca-bundle.crt", auth=auth) as client:
-            job_data = client.get(self.url + "/api/json").json()
-            builds = sorted(job_data["builds"], key=lambda x: x["number"])
-            self.latest_build_url = builds[-1]["url"]
+        job_data = self.client.get(self.url + "/api/json").json()
+        builds = sorted(job_data["builds"], key=lambda x: x["number"])
+        self.latest_build_url = builds[-1]["url"]
+        self.set_timer(3, self.update_latest_build, name="latest-build-update-timer")
 
     async def watch_latest_build_url(self, old_url, new_url):
-        self.update_build()
+        if len(new_url):
+            self.update_build()
 
     @work(exclusive=True)
     async def update_build(self) -> None:
         build_display = self.query_one("#build_display", Static)
-        
-        # Query the network API
-        auth = (os.environ["USERNAME"], os.environ["TOKEN"])
-        with httpx.Client(verify="/etc/ssl/certs/ca-bundle.crt", auth=auth) as client:
-            job_data = client.get(self.url + "/api/json").json()
-            builds = sorted(job_data["builds"], key=lambda x: x["number"])
-            latest_build = builds[-1]
-            latest_build_data = client.get(latest_build["url"] + "/wfapi/describe").json()
 
-            build_display.build = latest_build_data
-            print(latest_build_data["status"])
-        
-            if latest_build_data["status"] == "IN_PROGRESS":
-                self.set_timer(2.5, self.update_build, name="build-update-timer")
+        latest_build_data = self.client.get(self.latest_build_url + "/wfapi/describe").json()
+        build_display.build = latest_build_data
+    
+        if latest_build_data["status"] == "IN_PROGRESS":
+            self.set_timer(2.5, self.update_build, name="build-update-timer")
 
     def action_quit(self) -> None:
         self.exit()
 
-    async def action_refresh(self) -> None:
-        self.update_build(self.url)
+    async def action_build(self) -> None:
+        self.client.post(self.url + "/build?delay=0sec")
 
 
 if __name__ == "__main__":
