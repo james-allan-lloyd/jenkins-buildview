2303670280afa62a0916acfd8104a85e896e9cc1
James Lloyd <james.lloyd@tennet.eu>
2024-03-29 09:49:58 +0100

    Update the time since properly

--- jenkins-buildview.py
+++ jenkins-buildview.py
@@ -56,17 +56,29 @@ class BuildDisplay(Static):
         log.styles.display = "none"
         yield log
 
+    def on_mount(self) -> None:
+        self.set_interval(1, self.update_time_since_build, name="update-time-since-build-timer")
+
+    def update_root_label(self, tree, build):
+        if 'endTimeMillis' in build:
+            status_time = arrow.get(build['endTimeMillis']).humanize(only_distance=True)  # startTimeMillis, durationMills
+        elif 'startTimeMillis' in build:
+            status_time = arrow.get(build['endTimeMillis']).humanize(only_distance=True)  # startTimeMillis, durationMills
+        else:
+            status_time = ''
+
+        tree.root.label = f"Build {build['name']}: {build['status']} {status_time} ago"
+
+    def update_time_since_build(self) -> None:
+        if self.build is not None:
+            tree = self.query_one("#build_tree", Tree)
+            self.update_root_label(tree, self.build)
+
+
     def watch_build(self, old_build, new_build) -> None:
         tree = self.query_one("#build_tree", Tree)
         if new_build:
-            if 'endTimeMillis' in new_build:
-                status_time = arrow.get(new_build['endTimeMillis']).humanize(only_distance=True)  # startTimeMillis, durationMills
-            elif 'startTimeMillis' in new_build:
-                status_time = arrow.get(new_build['endTimeMillis']).humanize(only_distance=True)  # startTimeMillis, durationMills
-            else:
-                status_time = ''
-
-            tree.root.label = f"Build {new_build['name']}: {new_build['status']} {status_time} ago"
+            self.update_root_label(tree, new_build)
             tree.root.remove_children()
             tree.root.expand()
             for stage in new_build["stages"]:
